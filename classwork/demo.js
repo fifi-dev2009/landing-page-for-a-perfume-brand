33d00cf92fb61ce5e09bb05c49ba272435cf4e1d
fifi-dev2009 <kaiyewufiyinfoluwa@gmail.com>
2025-08-11 12:29:18 +0100

    Updated perfume brand landing page

---
+++ classwork/demo.js
@@ -0,0 +1,22 @@
+
+ let name ="Fiyin" ;
+ let age = 25;
+ let message ="hello";
+ 
+
+//  let name = "lolade";
+// let age = 46;
+// let message ="hola";
+// alert(10%5);
+// alert(8**5);
+
+// alert(`hello my name is ${name}`);
+// alert(message);
+// prompt("how are you doing");
+
+// CONST
+// const myBirthday ="25.10.2025";
+// alert(myBirthday);
+// alert(2==1);
+alert(5>=2);
+
