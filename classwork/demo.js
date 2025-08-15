469f9758bae7ef3cd83206394c657ddc110dfbd5
fifi-dev2009 <kaiyewufiyinfoluwa@gmail.com>
2025-08-15 14:34:32 +0100

    Update background toogle features

--- classwork/demo.js
+++ classwork/demo.js
@@ -1,7 +1,7 @@
 
- let name ="Fiyin" ;
- let age = 25;
- let message ="hello";
+//  let name ="Fiyin" ;
+//  let age = 25;
+//  let message ="hello";
  
 
 //  let name = "lolade";
@@ -18,5 +18,175 @@
 // const myBirthday ="25.10.2025";
 // alert(myBirthday);
 // alert(2==1);
-alert(5>=2);
+//alert(5>=2);
 
+
+//IF STATEMENT
+// let quiz = prompt( "What is the fastest animal?");
+// if(quiz=="paregrine falcon"){
+//     alert("You are right");
+//     alert("You deserve a round of applause");
+// }
+
+// IF ELSE STATEMENT
+// let quiz = prompt( "What is the fastest animal?");
+// if(quiz=="falcon"){
+//     alert("You are right");
+// }else{
+//     alert("Incorrect");
+// }
+
+// // ELSE IF STATEMENT
+// let year = prompt("How old are you");
+// if(year<18){
+//     alert("You are a minor")
+// }else if(year>=18){
+//     alert("You are an adult")
+// }else{
+//     alert("Try harder")
+// }
+
+// let accessAllowed;
+// let year = prompt("How old are you?");
+
+// if(year<18) {
+//     accessAllowed=  false;
+// } 
+// else{
+//     accessAllowed = true;
+// }
+// alert(accessAllowed)
+
+//  let year = prompt("How old are you");
+// if(year<12 && year >0){
+//     alert("You are a child")
+// }else if(year>13 && year<19 ){
+//     alert("You are a teenager")
+// }else if(year>19 &&  year <120){
+//     alert("You are an adult")
+// }
+// else{
+//     alert("I can't guess your age")
+// // }
+
+
+// let intro = " Welcome to Fiyin Quiz Competition";
+// alert(intro);
+
+// let quiz1=prompt("(1) What is the study of life? \nA:Biology \nB: Physics \nC: Science \nD: Ecology")
+// if (quiz1.toLowerCase() === "a") {
+//     alert("Correct")
+// } else {
+//     alert("Incorrect")
+// };
+
+
+// let quiz2= prompt(" (2) What is a female adult Dog called? \nA: Puppy \nB: Doggy \n C: Bitch \nD: Kid")
+// if (quiz2.toLowerCase() === "c") {
+//     alert("Correct")
+// } else {
+//     alert("Incorrect")
+// };
+
+ 
+// let quiz3 = prompt("(3) Sun rises in the ______ and sets in the _____. \nA: west,east \nB: west,north \nC: south,east \nD: east,west")
+// if (quiz3.toLowerCase() === "d") {
+//     alert("Correct")
+// } else {
+//     alert("Incorrect")
+// };
+
+
+//  let question = "The boy is as dark as a charcoal.";
+// let quiz4 = prompt( `(4)${question} What type of  figurative expression is this? \nA:Methaphor \nB:Simile \nC:Trony \nD:Hyberbole`);
+// if (quiz4.toLowerCase() === "b") {
+//     alert("Correct")
+// } else {
+//     alert("Incorrect")
+// };
+
+
+// let quiz5 = prompt("(5) Which one as the correct spelling ? \nA: Libary \nB: Library \nC: Libray \nD: Librey")
+// if (quiz5.toLowerCase() === "b") {
+//     alert("Correct")
+// } else {
+//     alert("Incorrect")
+// };
+
+
+//  let conclussion = "You are done with your Quiz Exam ";
+//  alert(conclussion);
+
+// NEW IDEA TO SOLVE THE PROBLEM
+// let quiz1 = document.getElementById("questions1");
+// if(quiz1 === "Biology"){
+//     alert("Correct")
+// }else{
+//     alert ("Incorrect")
+// }
+
+
+
+// 13th August, 2025 . Topic : LOOP//
+
+
+// 1) While loop 
+// a)for an "iNCREMMENT"//
+// let i = 0;
+// while(i<15){
+//     alert(i);
+//       i+=2;
+    
+// };
+
+// b)For "DECREMMENT"//
+// let i = 20;
+// while(i){
+//     alert(i);
+//       i-=2;
+    
+// }
+
+
+// 2) DO....WHILE LOOP//
+// do {
+//     alert(i);
+//     i+=2;
+// } while (i<10);
+
+//3)  FOR LOOP//
+// for(let i=0; i<10; i++){
+//     alert(i);
+// }
+
+//4) SWITCH 
+// let a = 2+2;
+// switch (a) {
+//     case 3 :
+//         alert("Lower");
+//         break;
+//     case 4:
+//         alert("Correct");
+//         break;
+
+//     case 5:
+//         alert("Higher");
+//         break;    
+
+//     default:
+//         alert("I do not know");
+//         break;
+// }
+
+
+
+// FUNCTIONS IN JAVASCRIPT
+//  function message(){
+//     alert('Are you sure you want to submit');
+//  }
+    
+
+
+function changeBackgroundColor() {
+   document.body.style.backgroundColor = "#000000b9";
+}
